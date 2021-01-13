3b15e79bc02fd4e9664bff27c7a47447e011740e
qtngr <57452200+qtngr@users.noreply.github.com>
2021-01-13 17:18:52 +0100

    add files

---
+++ dags/hello_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
+import os
+
+args = {
+    "owner": "airflow",
+    "start_date": datetime(2019, 3, 30),
+}
+
+
+def hello_world(**context):
+    hook = SnowflakeHook(snowflake_conn_id="sf_analytics_db")
+    print("hello world")
+
+    return True
+
+
+# Create the DAG with the parameters and schedule
+dag = DAG("hello_world", catchup=False, default_args=args, schedule_interval=None)
+
+
+with dag:
+    say_hello = PythonOperator(
+        task_id="hello_world", python_callable=hello_world, provide_context=True
+    )
+    say_hello2 = PythonOperator(
+        task_id="hello_world2", python_callable=hello_world, provide_context=True
+    )
+
+if __name__ == "__main__":
+    dag.clear(reset_dag_runs=True)
+    dag.run()
